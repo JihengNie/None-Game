6afc24115a335928514d04b50b615cc984e62765
Jiheng Nie <Niejiheng@gmail.com>
2022-09-21 04:42:33 +0000

    Updated a few puzzles and added hints

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 var $formSelector = document.querySelector('.question-form');
 var $question = document.querySelector('.question');
 var $resetButton = document.querySelector('.reset-button');
+var $hintsButton = document.querySelector('.hint-button');
+var $hintText = document.querySelector('.hints');
+var $hintCloseButton = document.querySelector('.close-hints');
+var $hintContainer = document.querySelector('.modal-background-container');
 var previousQuestionNumber = localStorage.getItem('None-Question-Number-local-storage');
 var counter = 0;
 
 window.addEventListener('beforeunload', storingQuestionNumber);
 $formSelector.addEventListener('submit', formSubmited);
 $resetButton.addEventListener('click', resetGame);
-
+$hintsButton.addEventListener('click', givingHints);
+$hintCloseButton.addEventListener('click', closingHints);
 if (previousQuestionNumber) {
   counter = JSON.parse(previousQuestionNumber);
   $question.textContent = questionArray[counter];
+}
+
+function closingHints(event) {
+  $hintContainer.className = 'modal-background-container hidden';
+}
+
+function givingHints(event) {
+  $hintText.textContent = hintsArray[counter];
+  $hintContainer.className = 'modal-background-container';
 }
 
 function resetGame(event) {
@@ -20,7 +34,7 @@ function resetGame(event) {
 
 function formSubmited(event) {
   event.preventDefault();
-  if ($formSelector.elements.input.value.toLocaleLowerCase() === answeArray[counter].toLocaleLowerCase()) {
+  if (removeSpaces($formSelector.elements.input.value.toLocaleLowerCase()) === removeSpaces(answeArray[counter].toLocaleLowerCase())) {
     counter++;
     if (counter === questionArray.length) {
       $question.textContent = 'Game Over';
@@ -34,4 +48,14 @@ function formSubmited(event) {
 function storingQuestionNumber(event) {
   var questionNumber = JSON.stringify(counter);
   localStorage.setItem('None-Question-Number-local-storage', questionNumber);
+}
+
+function removeSpaces(string) {
+  var result = '';
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      result += string[i];
+    }
+  }
+  return result;
 }
